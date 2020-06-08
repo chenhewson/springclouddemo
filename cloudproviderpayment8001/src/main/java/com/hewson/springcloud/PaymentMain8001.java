2943aba11eda107557af2d0eb39146027a9da8b7
chenhewson <hewson.chen@foxmail.com>
2020-06-08 11:46:52 +0800

    Initial commit

---
+++ cloudproviderpayment8001/src/main/java/com/hewson/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.hewson.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:    PaymentMain8001
+ * Package:    com.hewson.springcloud
+ * Description:
+ * Datetime:    2020/6/8   11:32
+ * Author:   hewson.chen@foxmail.com
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    //mainboot，快捷键自动补全
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
